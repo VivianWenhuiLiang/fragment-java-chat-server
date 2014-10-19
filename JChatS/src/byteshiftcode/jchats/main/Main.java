5834d909f22900430d4cb3846af4b33db8dd3ed6
Kim Mackey <kim.mackey.jr@gmail.com>
2014-10-19 14:31:34 -0500

    created main class with test output

---
+++ JChatS/src/byteshiftcode/jchats/main/Main.java
@@ -0,0 +1,13 @@
+package byteshiftcode.jchats.main;
+
+/**
+ * class run at start of program, will allow both arguments and console prompts
+ * to switch to client or server
+ * 
+ */
+public class Main {
+	public static void main(String[] args) {
+		// program starts here
+		System.out.println("Welcome to JChatS");
+	}
+}
